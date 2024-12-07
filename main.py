c1e59f7861313d8a2bc8f55ee535e0a2272a913f
Alexey Berezin <aliaksei.biarezin@ai-sft.dev>
2024-12-07 21:09:52 +0300

    added protected handler

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import uvicorn
-from fastapi import FastAPI
+from authx import AuthX, AuthXConfig
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi import Response
 
 app = FastAPI()
+
+authx_config = AuthXConfig()
+authx_config.JWT_ALGORITHM = "HS256"
+authx_config.JWT_SECRET_KEY = "SECRET_KEY"
+authx_config.JWT_ACCESS_COOKIE_NAME = "Authorization"
+authx_config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=authx_config)
+security.handle_errors(app)
 
 
 @app.get("/ping")
 async def ping():
     return "pong"
+
+
+@app.get('/login')
+async def login(username: str, password: str, response: Response):
+    if username == "test" and password == "test":
+        token = security.create_access_token(uid=username)
+        response.set_cookie(key=authx_config.JWT_ACCESS_COOKIE_NAME, value=token, httponly=True)
+        return
+    raise HTTPException(401, detail={"message": "Bad credentials"})
+
+
+@app.get("/protected", dependencies=[Depends(security.access_token_required)])
+async def protected():
+    return {"status": "ok"}
 
 
 if __name__ == '__main__':
